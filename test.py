bf3f05eb26f52fc48d23a26fc85171be10117ae0
vovkpavlik <vovk.pavlik@bk.ru>
2021-07-19 11:35:08 +0300

    тесты

---
+++ test.py
@@ -0,0 +1,102 @@
+import os
+
+import telegram
+from dotenv import load_dotenv
+from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (CallbackContext, CommandHandler, ConversationHandler,
+                          Filters, MessageHandler, CallbackQueryHandler, Updater)
+
+
+FIRST, SECOND = range(2)
+
+
+def start_handler(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+
+    keyboard = [
+        [InlineKeyboardButton("Первый стейт 1", callback_data="first_state_1")],
+        [InlineKeyboardButton("Первый стейт 2", callback_data="first_state_2")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = "Приветствую тебя в магазине 'crazy_fish_store'!\nНажми кнопку"
+
+
+    context.bot.send_message(
+        chat_id=chat_id, 
+        text=text,
+        reply_markup=reply_markup  
+    )
+    return FIRST
+
+
+def first_handler_one(update: Update, context: CallbackContext):
+
+    query = update.callback_query
+    query.answer()
+    
+    keyboard = [[InlineKeyboardButton("Второй стейт", callback_data="second_state")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = "first_handler_one!\nА теперь перейди ко второму!"
+    query.edit_message_text(text=text, reply_markup=reply_markup)
+
+    return SECOND
+
+
+def first_handler_two(update: Update, context: CallbackContext):
+
+    query = update.callback_query
+    query.answer()
+    
+    keyboard = [[InlineKeyboardButton("Второй стейт", callback_data="second_state")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = "first_handler_two!\nА теперь перейди ко второму!"
+    query.edit_message_text(text=text, reply_markup=reply_markup)
+
+    return SECOND
+
+
+def second_handler(update: Update, context: CallbackContext):
+
+    query = update.callback_query
+    query.answer()
+    
+    text = "Ты просто БОГ!"
+    query.edit_message_text(text=text)
+
+    return ConversationHandler.END
+
+
+
+def main():
+    load_dotenv()
+
+    tg_token = os.getenv("TG_TOKEN")
+    updater = Updater(token=tg_token)
+    dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start_handler)],
+
+        states = {
+            FIRST: [
+                CallbackQueryHandler(first_handler_one, "first_state_1"),
+                CallbackQueryHandler(first_handler_two, "first_state_2")
+            ],
+            SECOND: [CallbackQueryHandler(second_handler, "second_state")],
+      
+        },
+
+        fallbacks=[],
+
+    )
+    
+    dp.add_handler(conv_handler)
+ 
+    updater.start_polling()
+    
+if __name__ == "__main__":
+    main()
